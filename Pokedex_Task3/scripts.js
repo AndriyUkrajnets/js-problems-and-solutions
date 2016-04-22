43e4e290cefe810174c90fba4794f11cd57d7d75
AndriyUkrajnets <aukrajnets@gmail.com>
2016-04-22 16:08:01 +0200

    Task 3

---
+++ Pokedex_Task3/scripts.js
@@ -0,0 +1,37 @@
+var setNumber = function(e){
+  field = parseInt($("#field").val())
+  console.log(field)
+  e.preventDefault();
+}
+
+var div = function(id, name, weight) {
+    resalt = '<div class="item" data-order="' + weight + '">' + id + ".    " + name + ' - ' + weight +  '</div>'
+    return resalt;
+  }
+
+function pokeList(){
+  var visiblePokemonsCount = field;
+    for(var i = 1; i <= visiblePokemonsCount; i++){
+      $.get("https://pokeapi.co/api/v2/pokemon/"+i, function(data) {
+        var listOfPokemons = div(data.id, data.name, data.weight)
+        $("#list-of-pokemons").append(listOfPokemons);
+        })
+    }
+}
+
+function sort(){
+  var listOfPokemonsById = getSorted('.item', 'data-order')
+  $("#list-of-pokemons").html(listOfPokemonsById);
+}
+
+function getSorted(selector, attrName) {
+    return $($(selector).toArray().sort(function(a, b){
+        var aVal = parseInt(a.getAttribute(attrName)),
+            bVal = parseInt(b.getAttribute(attrName));
+        return bVal - aVal;
+    }));
+}
+
+function clean(){
+    $( "#list-of-pokemons" ).empty();
+  };
